9298a696545c2e5f5e66c4bf53a6b5e7e60e173a
Darshan S P <sdarshanpgowda2003@gmail.com>
2024-10-23 15:27:30 +0530

    blog addition

--- src/pages/Documents/Documents.js
+++ src/pages/Documents/Documents.js
@@ -1,27 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Documents.css'
 import folder from '../../assets/documents/Rectangle 32.png'
 import doc from '../../assets/documents/Rectangle 31.png'
 import Navbar from '../../components/navbar/Navbar'
+import axiosPrivate from '../../api/axios'
 
 function Documents() {
-    return (
-        <div className="documentsContainer">
-            <Navbar title='Chandrababu Gowda' value="Documents" />
-            <div className="docsContainer">
-                {
-                    [1, 2, 3, 4, 5].map((card, index) => {
-                        return (
-                            <div className="folders" key={Math.random()}>
-                                <img src={folder} alt="" />
-                                <p>{"College docs"}</p>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
-}
 
-export default Documents
+    const [document, setDocument] = React.useState([])
+
+    const fetchDocs = async () => {
+        await axiosPrivate.get('/directory').then(res => {
+            setDocument(res.data?.data?.documents || null)
+        })
+    }
+
+        useEffect(() => {
+            fetchDocs()
+        }, [])
+
+        return (
+            <div className="documentsContainer">
+                <Navbar title='Chandrababu Gowda' value="Documents" />
+                <div className="docsContainer">
+                    {
+                        document.map((card, index) => {
+                            return (
+                                <div className="folders" key={card._id} >
+                                    <img src={folder} alt="" />
+                                    <p>{card.name}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        )
+    }
+
+    export default Documents
